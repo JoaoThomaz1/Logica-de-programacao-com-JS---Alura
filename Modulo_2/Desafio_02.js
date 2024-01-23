2fa1ad079da832d4b92f5fb0e0d26a0782950513
João_Thomaz <joaovitor_rodriguesthomaz@hotmail.com>
2024-01-23 18:08:32 -0300

    Commit modulo 2 + Desafio 2

---
+++ Modulo_2/Desafio_02.js
@@ -0,0 +1,9 @@
+let diaSemana = prompt('Que dia da semana é hoje?');
+
+if (diaSemana == 'domingo' || diaSemana == 'sabado') {
+   alert('Bom fim de semana!');
+} else {
+   alert('Boa semana!');
+}
+
+let numero = prompt('Digite um numero, seja ne');
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10!');
 
 if (chute == numeroSecreto) {
-   alert('Isso aí! Você acertou o número secreto!');
+   alert(`Isso aí! Você acertou o número secreto ${numeroSecreto}!`);// primeira forma de juntar variavel com texto(para usar esse modo sem pre colocar o texto entre acrases ` )
 } else {
-   alert('Você  errou!');
+   alert('Você  errou! O número era ' + numeroSecreto);// segunda forma de juntar uma variavel com texto
 }
